6a9ae6be8f51724d02ae22c8de3d1b167278ab06
Oksana <oshporlyuk@gmail.com>
2021-12-12 15:34:47 -0800

    add basic registration test and registration validation test

--- elements/dropdown.js
+++ elements/dropdown.js
@@ -6,10 +6,12 @@ export default class Dropdown extends BaseElement {
         await this.wdioElement.click();
     }
 
+    async close() {
+        await browser.keys(['Escape']);
+    }
+
     async select(option) {
         await this.open();
         await new Button($(`//*[@class="mat-option-text"][contains(text(), "${option}")]`), `Question "${option}"`).click();
-        // find option
-        // click option
     }
 };
---
+++ test/specs/registration.validation.e2e.js
@@ -0,0 +1,34 @@
+import registrationPage from "../../pages/registration.page.js"
+
+describe('Register User', () => {
+    it('User does not enter any inputs on registration page', async () => {
+        // registration page
+        await registrationPage.open();
+
+        await registrationPage.emailInput.click();
+        await registrationPage.passwordInput.click();
+        await registrationPage.passwordRepeat.click();
+        await registrationPage.passwordAdvice.click();
+        await registrationPage.questionDropdown.open();
+        await registrationPage.questionDropdown.close();
+        await registrationPage.securityAnswer.click();
+        await registrationPage.registerButton.click();
+
+        await expect($('#mat-error-0')).toBeExisting();
+        await expect($('#mat-error-0')).toHaveTextContaining('Please provide an email address.');
+
+        await expect($('#mat-error-1')).toBeExisting();
+        await expect($('#mat-error-1')).toHaveTextContaining('Please provide a password.');
+
+        await expect($('#mat-error-2')).toBeExisting();
+        await expect($('#mat-error-2')).toHaveTextContaining('Please repeat your password.');
+
+        await expect($$('//mat-password-strength-info/mat-card/mat-card-content/div')).toBeElementsArrayOfSize(5);
+
+        await expect($('#mat-error-3')).toBeExisting();
+        await expect($('#mat-error-3')).toHaveTextContaining('Please select a security question.');
+
+        await expect($('#mat-error-4')).toBeExisting();
+        await expect($('#mat-error-4')).toHaveTextContaining('Please provide an answer to your security question.');
+    });
+});
