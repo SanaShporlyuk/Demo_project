b4eb545c4d039ceab75ea9bad799154c80f4926d
Oksana <oshporlyuk@gmail.com>
2021-12-12 15:50:35 -0800

    registration test check for success

--- test/specs/registration.positive.e2e.js
+++ test/specs/registration.positive.e2e.js
@@ -1,3 +1,4 @@
+import loginPage from "../../pages/login.page.js";
 import registrationPage from "../../pages/registration.page.js"
 
 describe('Register User', () => {
@@ -5,14 +6,17 @@ describe('Register User', () => {
         // registration page
         await registrationPage.open();
 
-        await registrationPage.emailInput.setValue('bla@bla.com');
+        // TODO: generate email
+        await registrationPage.emailInput.setValue('bla7@bla.com');
         await registrationPage.passwordInput.setValue('blabla3#_');
+        await registrationPage.passwordRepeat.setValue('blabla3#_');
 
         await registrationPage.questionDropdown.select(`Mother's maiden name?`);
         await registrationPage.securityAnswer.setValue('blabla');
 
         await registrationPage.registerButton.click();
 
-        browser.pause(5000);
+        await loginPage.waitForPageAvailable();
+        await expect(await browser.getUrl()).toMatch(`${browser.options.baseUrl}/#/login`);
     });
 });
