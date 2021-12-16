4327bc454bdb23e315e2dd37770a589794926e77
Oksana <oshporlyuk@gmail.com>
2021-12-15 20:52:46 -0800

    add before to login.positive test that creates user

--- test/specs/login.positive.e2e.js
+++ test/specs/login.positive.e2e.js
@@ -1,20 +1,37 @@
+import Chance from 'chance';
+import registrationPage from "../../pages/registration.page.js"
 import mainPage from "../../pages/main.page.js"
 import loginPage from "../../pages/login.page.js"
 
 describe('User login', () => {
+    const chance = new Chance();
+    const email = chance.email();
+    const password = 'blabla3#_';
+
+    before(async () => {
+        await registrationPage.open();
+        await registrationPage.emailInput.setValue(email);
+        await registrationPage.passwordInput.setValue(password);
+        await registrationPage.passwordRepeat.setValue(password);
+        await registrationPage.questionDropdown.select(`Mother's maiden name?`);
+        await registrationPage.securityAnswer.setValue('blabla');
+        await registrationPage.registerButton.click();
+        await loginPage.waitForPageAvailable();
+    });
+
     it('User does login with valid credentials', async () => {
         // main page
         await mainPage.open();
         await mainPage.openAccountMenu();
-        await mainPage.navigateToLogin();  
+        await mainPage.navigateToLogin();
 
         // login page
-        await loginPage.login('sanasmitrc@gmail.com', 'test123');              
+        await loginPage.login(email, password);
         await mainPage.accountMenuBtn.waitForExist();
         await mainPage.openAccountMenu();
-        
+
         await expect($('//div[@id = "mat-menu-panel-0"]//button[1]/span')).toBeExisting();
-        await expect($('//div[@id = "mat-menu-panel-0"]//button[1]/span')).toHaveTextContaining('sanasmitrc@gmail.com');
+        await expect($('//div[@id = "mat-menu-panel-0"]//button[1]/span')).toHaveTextContaining(email);
     });
 });
 
